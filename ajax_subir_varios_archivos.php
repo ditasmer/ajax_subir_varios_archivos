671119e28edbffc16b71a0e35307dbf17978bc1d
ditasmer <ditasmer@gmail.com>
2020-08-11 13:19:43 +0200

    seleccionar varios archivos, validar tamaños

--- ajax_subir_varios_archivos.php
+++ ajax_subir_varios_archivos.php
@@ -18,18 +18,32 @@
 			}
 
 			//recorrer el array de objetos para validar el tamaño de cada archivo seleccionado y confeccionar el objeto formData
-			archivos.forEach(function(archivo, clave){
-				//validar tamaño
+			var errores = 'los siguientes archivos superan los 900Kb:\n\n';
+			var hayError = false;
+			var datos = new FormData();
 
-				//encapsular archivo
+			Array.prototype.forEach.call(archivos, function(archivo, clave){
+				//validar tamaño de cada archivo
+				if(archivo.size > 900000){
+					errores += archivo.name + "\n";
+					hayError = true;
+				} else {
+					//encapsular archivo
+					var indice = 'archivo'+clave;
+					datos.append(indice, archivo);
+				}
+
 			})
+			if(hayError){
+				alert(errores);
+			}
 			
 		}
 	</script>
 </head>
 <body>
 <form enctype='multipart/form-data'>
-	<input type="file" id='fichero' ><br><br>
+	<input type="file" id='fichero' multiple><br><br>
 	<input type="button" id='enviar' value='enviar' onclick="enviarArchivo()">
 </form>
 </body></html>
